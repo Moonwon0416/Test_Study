71db7b9be032d4733c57ef15496534d280537ea7
LeeMoonWon <ansdnjs0416@naver.com>
2022-12-29 17:19:48 +0900

    첫 번째 서버

---
+++ simpleFlask.py
@@ -0,0 +1,15 @@
+#필요한 모듈을 단계적으로 import
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+# 테스트 기능
+@app.route('/hi')
+def hello_Flask():
+    return 'Hi, Flask'
+ 
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
